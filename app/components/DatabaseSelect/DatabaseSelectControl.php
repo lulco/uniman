5092584efc92d489e597da5906e13c95df16df5d
Michal Lulco <lulco@efabrica.sk>
2016-08-21 18:58:34 +0200

    Dvojstlpcovy layout

--- app/components/DatabaseSelect/DatabaseSelectControl.php
+++ app/components/DatabaseSelect/DatabaseSelectControl.php
@@ -4,6 +4,8 @@ namespace Adminerng\Components\DatabaseSelect;
 
 use Adminerng\Core\DriverInterface;
 use Nette\Application\UI\Control;
+use Nette\Application\UI\Form;
+use Tomaj\Form\Renderer\BootstrapVerticalRenderer;
 
 class DatabaseSelectControl extends Control
 {
@@ -24,5 +26,16 @@ class DatabaseSelectControl extends Control
         $this->template->actualDatabase = $this->database;
         $this->template->setFile(__DIR__ . '/default.latte');
         $this->template->render();
+    }
+    
+    protected function createComponentSelect()
+    {
+        $databases = array_keys($this->driver->databases());
+        $form = new Form();
+        $form->setRenderer(new BootstrapVerticalRenderer());
+        $form->addSelect('database', 'Select database', array_combine($databases, $databases))
+            ->setAttribute('onchange', 'window.location = \'' . $this->presenter->link('List:tables', $this->driver->type()) . '&database=\' + this.value')
+            ->setDefaultValue($this->database);
+        return $form;
     }
 }
